a57ca6bf5801f761a577e9e51d1ccb5579e1b104
MUSTAKIM PATVEKAR <44019216+mustakip@users.noreply.github.com>
2018-11-30 12:50:54 +0530

    Refactor test cases in gameOfLifeTest.js

--- test/gameOfLifeTest.js
+++ test/gameOfLifeTest.js
@@ -5,36 +5,40 @@ const contains = (list,element) => list.some(e=>e[0]===element[0] && e[1]===elem
 const isSame = (actualList,expectedList) => actualList.every(contains.bind(null,expectedList));
 const isSameArity = (actualList,expectedList) => actualList.length == expectedList.length;
 
-describe('nextGeneration',() => {
-  it('should generate an empty generation for a current generation that contains only one live cell',() => {
-    let currentGeneration = [[0,1]];
-    let bounds = {topLeft: [0,0], bottomRight: [3,3]};
-    let actualNextGen = nextGeneration(currentGeneration,bounds);
-    assert.deepEqual(actualNextGen,[]);
-  });
+describe("Test for nextGeneration Function " ,function(){
+  describe('nextGeneration',() => {
+    it('should generate an empty generation for a current generation that contains only one live cell',() => {
+      let currentGeneration = [[0,1]];
+      let bounds = {topLeft: [0,0], bottomRight: [3,3]};
+      let actualNextGen = nextGeneration(currentGeneration,bounds);
+      assert.deepEqual(actualNextGen,[]);
+    });
 
-  it('should generate a vertical blinker as the next step of a horizontal blinker',() => {
-    let currentGeneration = [[0,1],[1,1],[2,1]];
-    let expectedNextGen = [[1,0],[1,1],[1,2]]
-    let bounds = {topLeft: [0,0], bottomRight: [3,3]};
-    let actualNextGen = nextGeneration(currentGeneration,bounds);
-    assert.ok(isSame(actualNextGen,expectedNextGen));
-    assert.ok(isSameArity(actualNextGen,expectedNextGen));
-  });
+    it('should generate a vertical blinker as the next step of a horizontal blinker',() => {
+      let currentGeneration = [[0,1],[1,1],[2,1]];
+      let expectedNextGen = [[1,0],[1,1],[1,2]]
+      let bounds = {topLeft: [0,0], bottomRight: [3,3]};
+      let actualNextGen = nextGeneration(currentGeneration,bounds);
+      assert.ok(isSame(actualNextGen,expectedNextGen));
+      assert.ok(isSameArity(actualNextGen,expectedNextGen));
+    });
 
-  it('should kill cells not within bounds',() => {
-    let currentGeneration = [[0,1],[0,2],[0,3]];
-    let expectedNextGen = []
-    let bounds = {topLeft: [1,1], bottomRight: [3,3]};
-    let actualNextGen = nextGeneration(currentGeneration,bounds);
-    assert.ok(isSame(actualNextGen,expectedNextGen));
-    assert.ok(isSameArity(actualNextGen,expectedNextGen));
+    it('should kill cells not within bounds',() => {
+      let currentGeneration = [[0,1],[0,2],[0,3]];
+      let expectedNextGen = []
+      let bounds = {topLeft: [1,1], bottomRight: [3,3]};
+      let actualNextGen = nextGeneration(currentGeneration,bounds);
+      assert.ok(isSame(actualNextGen,expectedNextGen));
+      assert.ok(isSameArity(actualNextGen,expectedNextGen));
+    });
+
+    it("should work for behive pattern ",function(){
+      let currentGeneration = [[1,2],[1,3],[2,1],[2,4],[3,2],[3,3]];
+      let expectedNextGen = [[1,2],[1,3],[2,1],[2,4],[3,2],[3,3]];
+      let bounds = {topLeft:[-3,-3],bottomRight:[4,5]};
+      let actualNextGen = nextGeneration(currentGeneration,bounds);
+      assert.ok(isSame(actualNextGen,expectedNextGen));
+      assert.ok(isSameArity(actualNextGen,expectedNextGen));
+    });
   });
 });
-
-describe("new test " ,function(){
-  it("behive",function(){
-    assert.deepEqual(nextGeneration([[0,0], [1,0], [2,0]], {topLeft : [0,0], bottomRight: [3,3]}), [[1,0], [1,1]]);
-    assert.deepEqual(nextGeneration([[1,2],[1,3],[2,1],[2,4],[3,2],[3,3]],{topLeft:[-3,-3],bottomRight:[4,5]}),[[1,2],[1,3],[2,1],[2,4],[3,2],[3,3]]);
-});
-  });
