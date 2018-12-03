c82bbfdd0fd9f47bc1bc237ed78d8c2007201532
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-03 04:10:42 +0000

    Deploy STEP-tw/gol-mustakip to github.com/STEP-tw/gol-mustakip.git:gh-pages

---
+++ precache-manifest.9fbb820cfaf92f5de7f65ae9e3d130bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c1086aaba313ea5cb40",
+    "url": "/gol-mustakip/static/js/runtime~main.9c1086aa.js"
+  },
+  {
+    "revision": "05d4bd77bc9ca3437b62",
+    "url": "/gol-mustakip/static/js/main.05d4bd77.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-mustakip/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "05d4bd77bc9ca3437b62",
+    "url": "/gol-mustakip/static/css/main.5490fdf0.chunk.css"
+  },
+  {
+    "revision": "b29ae60f41c9ed06e6ef14abd426c78a",
+    "url": "/gol-mustakip/index.html"
+  }
+];
